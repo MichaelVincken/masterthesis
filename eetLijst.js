ea6a6635c8d1e811958f1a9aacc247615a83f170
MichaelVincken <michaelvinckenbe@gmail.com>
2015-11-14 16:38:16 +0100

    Basic Food Diary: DONE
    Basic functionalities like removing and substituting a meal in the food
    diary.

--- eetLijst.js
+++ eetLijst.js
@@ -1,21 +1,13 @@
 /**
  * Created by Michael on 12/11/2015.
  */
-$(document).ready(function(){
+function verwijder(btn) {
+  var row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+}
 
-    function testFunction(){
-        $(".doel").hide();
-
-    };
-    
-$("#voedingsdagboek tr").click(function(){
-   $(this).addClass('selected').siblings().removeClass('selected');    
-   var value=$(this).find('td:nth-child(2)').html();
-   alert(value);    
-});
-
-$('.ok').on('click', function(e){
-    alert($("#voedingsdagboek tr.selected td:nth-child(3)").html());
-});
-
-});
+function alternatief(btn) {
+  var row = btn.parentNode.parentNode.cells;
+  my_text=prompt('Geef een vervangerecht op:');
+  row[1].innerHTML = "<td>" + my_text + "</td>";
+}
